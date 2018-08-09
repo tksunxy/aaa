027ce6d700fe2fc4c91a8f25432d293a27d45230
王子豪 <zhwang@kunfangtech.cn>
2018-08-09 18:39:26 +0800

    1.临时提交

--- src/main/java/com/github/netty/util/HostUtil.java
+++ src/main/java/com/github/netty/util/HostUtil.java
@@ -1,7 +1,10 @@
 package com.github.netty.util;
 
+import java.net.NetworkInterface;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.Enumeration;
+import java.util.StringJoiner;
 
 /**
  * Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
@@ -27,6 +30,10 @@ public class HostUtil {
 
             return Boolean.getBoolean("com.sun.javafx.isEmbedded");
         });
+    }
+
+    public static String getOsName() {
+        return osName;
     }
 
     public static boolean is64Bit() {
@@ -57,4 +64,62 @@ public class HostUtil {
     public static boolean isEmbedded() {
         return embedded;
     }
+
+    /**
+     * mac地址
+     * @return 例( ‎00-24-7E-0A-22-93)
+     */
+    public static String getMac() {
+        try {
+            Enumeration<NetworkInterface> el = NetworkInterface
+                    .getNetworkInterfaces();
+            while (el.hasMoreElements()) {
+                byte[] mac = el.nextElement().getHardwareAddress();
+                if (mac == null || mac.length <= 0) {
+                    continue;
+                }
+                String hexstr = bytesToHexString(mac);
+                return getSplitString(hexstr, "-", 2).toUpperCase();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String getSplitString(String str, String split, int length) {
+        if(str == null || split.length() == 0){
+            return "";
+        }
+        int len = str.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            if (i % length == 0 && i > 0) {
+                sb.append(split);
+            }
+            sb.append(str.charAt(i));
+        }
+
+        String[] attrs = sb.toString().split(split);
+        StringJoiner stringJoiner = new StringJoiner(split);
+        for (String attr : attrs) {
+            stringJoiner.add(attr);
+        }
+        return stringJoiner.toString();
+    }
+
+    private static String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = 0; i < src.length; i++) {
+            int v = src[i] & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+        return stringBuilder.toString();
+    }
+
 }
